0759259461d5ac0009e1c315ce5c12e7ff996210
Dante <81756167+Danylo690@users.noreply.github.com>
2022-12-07 22:19:57 +0200

    Added main page

---
+++ PCWPF/Views/FTPConnectView.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PCWPF.Views
+{
+    /// <summary>
+    /// Interaction logic for FTPConnectView.xaml
+    /// </summary>
+    public partial class FTPConnectView : Window
+    {
+        public FTPConnectView()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void MinimizeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void CloseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
